0689dba0b27ef2bc57a08df23f8809e070d3fd07
JavierBla <95310950+JavierBla@users.noreply.github.com>
2023-02-02 20:35:57 +0100

    Create agregar_usuarios_grupos.sh

---
+++ Scripts proyecto/agregar_usuarios_grupos.sh
@@ -0,0 +1,7 @@
+usermod -a -G Publico_rx,ESO_rx,1ESO_rwx usu_1ESO
+usermod -a -G Publico_rx,ESO_rx,2ESO_rwx usu_2ESO
+usermod -a -G Publico_rx,ESO_rx,3ESO_rwx usu_3ESO
+usermod -a -G Publico_rx,ESO_rx,4ESO_rwx usu_4ESO
+usermod -a -G Publico_rx,BACH_rx,1BACH_rwx usu_1BACH
+usermod -a -G Publico_rx,BACH_rx,2BACH_rwx usu_2BACH
+usermod -a -G Publico_rwx,DAM_rx,DAM_rwx usu_DAM
