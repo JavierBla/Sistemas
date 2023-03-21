57f57b66e74046a31e2a4545d5c90942d568b2c7
JavierBla <95310950+JavierBla@users.noreply.github.com>
2023-03-21 19:38:32 +0100

    Update imagenes.sh

--- imagenes.sh
+++ imagenes.sh
@@ -1,5 +1,6 @@
 Naci24¿
 
+#!/bin/bash
 wget https://www.aemet.es/imagenes_d/eltiempo/prediccion/mapa_frentes/2023032112+000_ww_gpx0a200.gif
 wget https://www.aemet.es/imagenes_d/eltiempo/prediccion/mapa_frentes/2023032112+012_ww_g1x0a212.gif
 wget https://www.aemet.es/imagenes_d/eltiempo/prediccion/mapa_frentes/2023032112+024_ww_g1x0a2d1.gif
