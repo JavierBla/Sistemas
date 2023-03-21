69702e77c8a8c0821c0d92f4985021c9efa3e22c
JavierBla <95310950+JavierBla@users.noreply.github.com>
2023-03-21 19:36:38 +0100

    Create imagenes.sh

---
+++ imagenes.sh
@@ -0,0 +1,8 @@
+Naci24¿
+
+wget https://www.aemet.es/imagenes_d/eltiempo/prediccion/mapa_frentes/2023032112+000_ww_gpx0a200.gif
+wget https://www.aemet.es/imagenes_d/eltiempo/prediccion/mapa_frentes/2023032112+012_ww_g1x0a212.gif
+wget https://www.aemet.es/imagenes_d/eltiempo/prediccion/mapa_frentes/2023032112+024_ww_g1x0a2d1.gif
+wget https://www.aemet.es/imagenes_d/eltiempo/prediccion/mapa_frentes/2023032112+036_ww_g1x0a2c1.gif
+wget https://www.aemet.es/imagenes_d/eltiempo/prediccion/mapa_frentes/2023032112+048_ww_g1x0a2d2.gif
+wget https://www.aemet.es/imagenes_d/eltiempo/prediccion/mapa_frentes/2023032112+060_ww_g1x0a2c2.gif
